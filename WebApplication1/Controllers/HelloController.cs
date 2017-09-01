d24a3f8db6adc5a711907f845de38b87b108873b
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-09-01 10:39:07 +0530

    Hello World

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello world page.";
+            return View();
+        }
+    }
+}
